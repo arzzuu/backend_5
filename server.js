8645eb64e33a5a142a7e6ae32848b66e849c2570
arzzuu <arzzuu>
2022-12-15 00:31:38 +0400

    change something and add something

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 
-const users = [ 
-  { "number": 1234123412341234, "id": "123" },
-  { "number": 2345234523452345, "id": "546" },
-  { "number": 8907890789078907, "id": "101" },
-  { "number": 5678567856785678, "id": "1" },
-  { "number": 6789678967896789, "id": "25" },
+const cards = [ 
+  { number: "1234123412341234", id: 123 },
+  { number: "2345234523452345", id: 546 },
+  { number: "8907890789078907", id: 101 },
+  { number: "5678567856785678", id: 1 },
+  { number: "6789678967896789", id: 25 },
  ];
-
-app.get('/card/:id', function (req, res) {
-  res.send(JSON.stringify(users.find((user) => user.id == req.params.id)));
+ app.get('/card/:id', function (req, res) {
+  res.send(JSON.stringify(cards.find((card) => card.id == req.params.id)));
+});
+app.get("/card/:id", function (req, res) {
+  const idOfcard = parseInt(req.params.id);
+  const card = cards.find((card) => card.id === idOfcard);
+  if (!card) {
+    res.status(404).send();
+  }
+  res.status(200).json(card);
 });
 
 app.listen(3000, function () {
